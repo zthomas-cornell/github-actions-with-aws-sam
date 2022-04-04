e73ab8f6ab0e59540fe35d7051ea7cddf9ac38a9
Zak Thomas <zthomas@cornellpump.com>
2022-04-04 11:06:20 -0700

    Test 2.

--- hello_world/app.py
+++ hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world 1",
+                "message": "hello world 2",
                 # "location": ip.text.replace("\n", "")
             }
         ),
