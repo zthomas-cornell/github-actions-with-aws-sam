ae1c8f5c8a17435951b6ef5cb2bcaed8a3a06067
Zak Thomas <zthomas@cornellpump.com>
2022-04-05 11:11:37 -0700

    Test.

--- hello_earth/app.py
+++ hello_earth/app.py
@@ -6,5 +6,5 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "hello earth 💒"}),
+        "body": "hello earth 💒",
     }
--- hello_mars/app.py
+++ hello_mars/app.py
@@ -6,5 +6,5 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "hello mars 💒"}),
+        "body": "hello mars 💒",
     }
